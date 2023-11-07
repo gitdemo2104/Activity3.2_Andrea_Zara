7309b3365bf0901efbcd5046459d81069e47272a
ANDREA_ZARA <zaraandrea57@gmail.com>
2023-11-07 14:02:07 +0800

    feat add-column-price syntax

--- hero.sql
+++ hero.sql
@@ -151,4 +151,6 @@ VALUES
     (7, 7, 107),  -- Franco has Necklace of Durance
     (8, 8, 108),  -- Fanny has Thunder Belt
     (9, 9, 109),  -- Leomord has Blade Armor
-    (10, 10, 110);  -- Chou has Feather of Heaven
+    (10, 10, 110);  -- Chou has Feather of Heavens
+ALTER TABLE item
+ADD item_price DECIMAL (10,5);
